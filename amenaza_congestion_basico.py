0eaa7f6e85d7bcd5f3471c5b95ae97b5b71db5b0
Mateorodri88 <mateoguez88@gmail.com>
2024-01-31 12:12:15 -0500

    Se cambio el numero de simulaciones a 15000

--- amenaza_congestion_basico.py
+++ amenaza_congestion_basico.py
@@ -3,7 +3,7 @@ import random
 # Parámetros
 volumen_maximo = 500  # Volumen máximo de vehículos por hora
 capacidad_via = 400   # Capacidad de la vía en vehículos por hora
-simulaciones = 10000  # Número de simulaciones
+simulaciones = 15000  # Número de simulaciones
 
 # Contador para las veces que el volumen de tráfico excede la capacidad de la vía
 excesos = 0
